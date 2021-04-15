7a0a323e5610a69b418eda31330cad1a8cf6a1f3
Zaino <hello@darrin.dev>
2021-04-14 17:12:45 -0700

    add pseudocode

---
+++ index.js
@@ -0,0 +1,22 @@
+//* Install the required packages
+    //* inquirer 
+    //* axios - review activity 09-28
+
+//* Define variables for:
+    //* const inquirer 
+    //* const fs = require
+    //* const axios for get 
+
+
+//* Create an array of questions for user input
+    //* prompt the user for Description, Installation, Usage, Contributing, and Tests
+    //* specify the input type 
+        //* see https://www.npmjs.com/package/inquirer
+
+
+//* Create a function to write to the generated README file
+    //* using fs.writefile 
+
+
+//* In a function initialize the inquirer package 
+
